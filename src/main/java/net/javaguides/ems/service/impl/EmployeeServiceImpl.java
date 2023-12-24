87105fd7652eb7e3db24d3356a0f5e9e673469e3
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2023-12-24 09:28:35 +0700

    Build Delete REST API

--- src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -51,5 +51,11 @@ public class EmployeeServiceImpl implements EmployeeService {
         return EmployeeMapper.mapToEmployeeDto(updateEmployeeObj);
     }
 
-
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee is not exists with given id" + employeeId)
+        );
+        employeeRepository.deleteById(employeeId);
+    }
 }
